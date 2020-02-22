4c0176a5487ddd11a5b9a83d2248abb504e32da5
Oriol Canalias <oriol.canalias@wavy.global>
2020-02-22 17:38:33 -0300

    sentinel

--- master-slave/src/main/kotlin/br/com/devcave/redis/controller/RedisController.kt
+++ master-slave/src/main/kotlin/br/com/devcave/redis/controller/RedisController.kt
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("redis")
+@RestController
+@RequestMapping("redis")
 class RedisController(
     private val redisRepository: RedisRepository
 ) {
